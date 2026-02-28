dfe36d8f23486da9b91a8c67e1b238540949693a
Rakshitha L Hegde <rakshithalhegde1ga21cs119@gmail.com>
2026-02-28 11:44:30 +0530

    Configure ESLint and Prettier for backend

---
+++ backend/eslint.config.mjs
@@ -0,0 +1,33 @@
+import js from "@eslint/js";
+import globals from "globals";
+import tseslint from "typescript-eslint";
+import prettier from "eslint-plugin-prettier";
+import prettierConfig from "eslint-config-prettier";
+import { defineConfig } from "eslint/config";
+
+export default defineConfig([
+  {
+    files: ["**/*.{js,mjs,cjs,ts,mts,cts}"],
+    plugins: {
+      js,
+      prettier,
+    },
+    extends: ["js/recommended"],
+    languageOptions: {
+      globals: globals.node,
+    },
+    rules: {
+      "prettier/prettier": "error",
+    },
+  },
+
+  {
+    files: ["**/*.js"],
+    languageOptions: {
+      sourceType: "commonjs",
+    },
+  },
+
+  tseslint.configs.recommended,
+  prettierConfig,
+]);
